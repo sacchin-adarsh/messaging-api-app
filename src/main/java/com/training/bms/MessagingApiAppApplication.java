56df1d6c686a676fe9521e5d46da0181d45ebafb
sacchin-adarsh <sacchinadarsh@gmail.com>
2024-07-12 15:16:27 +0530

    testing

--- src/main/java/com/training/bms/MessagingApiAppApplication.java
+++ src/main/java/com/training/bms/MessagingApiAppApplication.java
@@ -2,11 +2,8 @@ package com.training.bms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class MessagingApiAppApplication {
 
 	public static void main(String[] args) {
@@ -18,10 +15,5 @@ public class MessagingApiAppApplication {
 	public static void printGreetings()
 	{
 		System.out.println("Hi Oracle!!!");
-	}
-	
-	@GetMapping("enjoyWeekend")
-	public String printName() {
-		return "Happy Weekend Sacchin ";
 	}
 }
